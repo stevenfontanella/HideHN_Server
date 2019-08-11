7993908eddcc3ffe004204e0c32e367134911e9d
stevenfontanella <steven.fontanella@gmail.com>
2019-08-11 14:52:54 -0700

    Take in list of ids and return list of bools

--- sentiment/views.py
+++ sentiment/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
 
 import requests
 
+'''
+right to left composition for unary functions
+'''
+def compose(*fns):
+    def inner(x):
+        curr = x
+        for f in reversed(fns):
+            curr = f(curr)
+        return curr
+    return inner
+
 def get_hn_post(id):
     # TODO: validate id
-    # TODO: id should be part of path, not param
+
     # https://stackoverflow.com/questions/30967822/when-do-i-use-path-params-vs-query-params-in-a-restful-api
 
+    # this is a bottleneck, but much faster than loading the actual website
+    # with post caching it should be reasonable
     # hn = requests.get(f"https://hacker-news.firebaseio.com/v0/item/{id}.json").json()
+    return None
 
-    ...
-    # print(hn)
+    return hn
 
 a = True
 def should_filter(json):
@@ -21,12 +34,13 @@ def should_filter(json):
 
 def index(request):
     try:
-        id = request.GET["id"]
+        ids = request.GET.getlist("id")
+        print(ids)
     except KeyError:
-        return HttpResponseBadRequest("No id given")
+        return HttpResponseBadRequest("No ids given")
 
-    json = get_hn_post(id)
-    should = int(should_filter(json))
-    print(should)
-    return HttpResponse(should)
+    sentiment_list = list(map(compose(int, should_filter, get_hn_post), ids))
+    print(sentiment_list)
+
+    return JsonResponse(sentiment_list, safe=False)
 
